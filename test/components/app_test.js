8f3abae1bab297ad238e4e97fb6c34acdf4a8273
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-01-09 17:06:32 +0100

    describe - it - expect

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,6 +1,7 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
+// Use 'describe' to group together similar tests
 describe('App' , () => {
   let component;
 
@@ -8,7 +9,9 @@ describe('App' , () => {
     component = renderComponent(App);
   });
 
+// Use 'it' to test a single attribute of a target
   it('renders something', () => {
+    // Use 'expect' to make an 'assertion' about a target
     expect(component).to.exist;
   });
 });
